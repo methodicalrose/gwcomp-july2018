0ad836b9de0259d4dd1216669aaaa924e5aefff4
Andy Callahan <amandafillpot@icloud.com>
2018-07-24 21:47:48 -0700

    prompt generator added

---
+++ js/new-project.js
@@ -0,0 +1,115 @@
+let promptOne = randomNumber();
+let promptTwo = randomNumber();
+let promptThree = randomNumber();
+
+switch(promptOne) {
+  case 0:
+    promptOne = "apple";
+    break;
+  case 1:
+    promptOne = "ring";
+    break;
+  case 2:
+    promptOne = "bear";
+    break;
+  case 3:
+    promptOne = "giant";
+    break;
+  case 4:
+    promptOne = "wish";
+    break;
+  case 5:
+    promptOne = "seed";
+    break;
+  case 6:
+    promptOne = "miracle";
+    break;
+  case 7:
+    promptOne = "teacup";
+    break;
+  case 8:
+    promptOne = "mirror";
+    break;
+  case 9:
+    promptOne = "old house";
+    break;
+  default:
+    alert("Error");
+    break;
+}
+switch(promptTwo) {
+  case 0:
+    promptTwo = "cat";
+    break;
+  case 1:
+    promptTwo = "glove";
+    break;
+  case 2:
+    promptTwo = "child";
+    break;
+  case 3:
+    promptTwo = "forest";
+    break;
+  case 4:
+    promptTwo = "violin";
+    break;
+  case 5:
+    promptTwo = "siren";
+    break;
+  case 6:
+    promptTwo = "laugh";
+    break;
+  case 7:
+    promptTwo = "hiccup";
+    break;
+  case 8:
+    promptTwo = "fairy";
+    break;
+  case 9:
+    promptTwo = "sword";
+    break;
+  default:
+    alert("Error");
+    break;
+}
+switch(promptThree) {
+  case 0:
+    promptThree = "hurricane";
+    break;
+  case 1:
+    promptThree = "curse";
+    break;
+  case 2:
+    promptThree = "reckoning";
+    break;
+  case 3:
+    promptThree = "ghost";
+    break;
+  case 4:
+    promptThree = "breakup";
+    break;
+  case 5:
+    promptThree = "knife";
+    break;
+  case 6:
+    promptThree = "sheets";
+    break;
+  case 7:
+    promptThree = "grandfather clock";
+    break;
+  case 8:
+    promptThree = "howl";
+    break;
+  case 9:
+    promptThree = "tattoo";
+    break;
+  default:
+    alert("Error");
+    break;
+}
+
+let writingPrompt = `The ${promptOne}, the ${promptTwo}, and the ${promptThree}.`;
+
+let promptGenerator = document.createElement("h2");
+promptGenerator.textContent = writingPrompt;
+generatorDiv.appendChild(promptGenerator);
---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+const generatorDiv = document.querySelector(".generator");
+
+const randomNumber = () => {
+  Math.floor(Math.random() * 10);
+};
